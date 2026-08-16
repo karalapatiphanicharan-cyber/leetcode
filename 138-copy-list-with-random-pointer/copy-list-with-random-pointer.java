5d044db29dd317e03cad1b1509f7636c1f528f03
Phani Charan <karalapatiphanicharan@gmail.com>
2026-08-16 12:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (14.75%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public Node copyRandomList(Node head) 
+    {
+        if(head == null)
+        return null;
+
+        HashMap<Node,Node> map = new HashMap<>();
+        Node current = head;
+        while(current!=null)
+        {
+            Node copy = new Node(current.val);
+            map.put(current,copy);
+            current = current.next;
+        }
+        current = head;
+        while(current!=null)
+        {
+           Node copy = map.get(current);
+           copy.next = map.get(current.next);
+           copy.random = map.get(current.random);
+           current = current.next;
+        }
+        return map.get(head);
+    }
+}
